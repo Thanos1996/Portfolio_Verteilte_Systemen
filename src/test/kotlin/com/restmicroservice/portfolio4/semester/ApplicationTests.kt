3348fcf364ca660ece292f6eee45ff3dddab195b
Thanos1996 <91094288+Thanos1996@users.noreply.github.com>
2024-02-21 11:45:14 +0100

    created Project for Rest Microservice using Springboot

---
+++ src/test/kotlin/com/restmicroservice/portfolio4/semester/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.restmicroservice.portfolio4.semester
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
